8f586d6530f8f42798f376bd2f168335d526bc26
Shadman Chowdhury <chowdhuryshadman0@gmail.com>
2022-10-21 15:18:38 -0400

    frontend init

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+//Views
+import Hello from './views/hello_world.jsx';
+
+
+//Styles
+import './style.scss';
+
+const App = () => {
+  return (
+    <>
+      <Hello />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/views/hello_world.jsx
@@ -0,0 +1,8 @@
+const Hello = () => {
+    return (
+        <h1>CA Austin on Duty</h1>
+    );
+  }
+  
+  export default Hello;
+  
