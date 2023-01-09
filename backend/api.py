63a6975c8ca704be7200b2d00b678854d9e6326f
Shadman Chowdhury <chowdhuryshadman0@gmail.com>
2023-01-08 22:09:16 -0500

    allow all origin

--- backend/api.py
+++ backend/api.py
@@ -12,17 +12,18 @@ import datetime
 
 
 origins = [
-    "http://localhost",
-    "http://localhost:3000",
-    "http://localhost:8000",
-    "http://localhost:8080",
-    "http://localhost:3001",
-    "http://frontend:3000",
-    "http://frontend:3001",
-    "http://backend:8000",
-    "http://backend:8080",
-    "http://206.81.9.249/",
-    "http://206.81.9.249/*",
+    # "http://localhost",
+    # "http://localhost:3000",
+    # "http://localhost:8000",
+    # "http://localhost:8080",
+    # "http://localhost:3001",
+    # "http://frontend:3000",
+    # "http://frontend:3001",
+    # "http://backend:8000",
+    # "http://backend:8080",
+    # "http://206.81.9.249/",
+    # "http://206.81.9.249/*",
+    '*'
 
 ]
 
