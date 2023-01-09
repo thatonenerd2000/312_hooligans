a8cc4b9e8e9ac637e9bb31ca36dd68dc018556c3
Shadman Chowdhury <chowdhuryshadman0@gmail.com>
2023-01-09 01:43:22 -0500

    disabled nginx

--- backend/api.py
+++ backend/api.py
@@ -18,7 +18,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    # allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 
